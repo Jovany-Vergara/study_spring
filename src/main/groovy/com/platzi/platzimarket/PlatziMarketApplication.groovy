0408060583899c92fb744e0150ab1c207e9898a1
Jovany Vergara <jovany@makingdevs.com>
2022-03-22 12:20:52 -0600

    [master] agregando mi proyecto a git

---
+++ src/main/groovy/com/platzi/platzimarket/PlatziMarketApplication.groovy
@@ -0,0 +1,13 @@
+package com.platzi.platzimarket
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PlatziMarketApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PlatziMarketApplication, args)
+	}
+
+}
